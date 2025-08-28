d1764a37276087c952ecc66effca044558f8d0f6
myhomesbudget-cmd <myhomesbudget@gmail.com>
2025-08-28 23:15:18 +0200

    Update supabase-client.js

--- it/supabase-client.js
+++ it/supabase-client.js
@@ -1,12 +1,10 @@
-<script type="module">
 // === CONFIGURA QUI con i tuoi valori ===
 const SUPABASE_URL = "https://upcusjlumfeadgbhziwq.supabase.co";       // il tuo Project URL
-const SUPABASE_ANON_KEY = "sb_publishable_HmEbBow...";                 // la tua Publishable Key
+const SUPABASE_ANON_KEY = "sb_publishable_HmEbBowyomvWuvAaSzt0bw_zzAWL_Q2";                  // la tua Publishable Key
 // ======================================
 
 // Importa la libreria Supabase
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-// Esporta client da riusare in altre pagine
+// Esporta il client da riusare in altre pagine
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-</script>
